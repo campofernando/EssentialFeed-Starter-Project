e9c9120f2d14eb00fc19708117641fc8eea8d25e
Fernando Campo <campofernando594@gmail.com>
2024-12-22 12:13:04 -0300

    Remove default value for completion block (moved it to the test)

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -21,7 +21,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         let (sut, client) = makeSut(url: url)
         
-        sut.load()
+        sut.load { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -31,8 +31,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         let (sut, client) = makeSut(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load() { _ in }
+        sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
