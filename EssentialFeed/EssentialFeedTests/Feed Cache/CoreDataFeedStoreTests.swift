776a5dd0d92ffd82bac0d6625a385678299126e9
Fernando Campo <campofernando594@gmail.com>
2025-01-25 13:45:18 -0300

    Add empty CoreData feed store specs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Fernando Campo Garcia on 25/01/25.
+//
+
+import Testing
+
+struct CoreDataFeedStoreTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
