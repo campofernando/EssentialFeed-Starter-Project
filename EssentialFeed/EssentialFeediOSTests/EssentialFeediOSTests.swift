9123d363755b6ead164b714d93f2c7aa132d58c5
Fernando Campo <campofernando594@gmail.com>
2025-03-28 19:25:16 -0300

    Add EssentialFeediOS framework (prod and test) targets for iOS platform-specific components

---
+++ EssentialFeed/EssentialFeediOSTests/EssentialFeediOSTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeediOSTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Fernando Campo Garcia on 28/03/25.
+//
+
+import XCTest
+@testable import EssentialFeediOS
+
+final class EssentialFeediOSTests: XCTestCase {
+}
