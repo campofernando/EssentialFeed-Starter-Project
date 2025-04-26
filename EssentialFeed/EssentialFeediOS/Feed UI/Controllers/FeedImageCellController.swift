c54d9aa6f8aca3a8cac72909fe2a28d892ad7d24
Fernando Campo <campofernando594@gmail.com>
2025-04-26 10:51:12 -0300

    Extract FeedImageCell creation/configuration and image loading logic with FeedImageDataLoader to the new FeedImageCellController

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -0,0 +1,58 @@
+//
+//  FeedImageCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Fernando Campo Garcia on 26/04/25.
+//
+
+import Foundation
+import EssentialFeed
+import UIKit
+
+final class FeedImageCellController {
+    private var task: FeedImageDataLoaderTask?
+    private let model: FeedImage
+    private let imageLoader: FeedImageDataLoader
+    
+    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+        self.model = model
+        self.imageLoader = imageLoader
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = FeedImageCell()
+        cell.locationContainer.isHidden = (model.location == nil)
+        cell.locationLabel.text = model.location
+        cell.descriptionLabel.text = model.description
+        cell.feedImageView.image = nil
+        cell.feedImageRetryButton.isHidden = true
+        cell.feedImageContainer.startShimmering()
+        
+        let loadImage = { [weak self, weak cell] in
+            guard let self else { return }
+            
+            task = imageLoader.loadImageData(from: model.url) { [weak cell] result in
+                if case .success(let data) = result,
+                   let image = UIImage(data: data) {
+                    cell?.feedImageView.image = image
+                } else {
+                    cell?.feedImageRetryButton.isHidden = false
+                }
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
+        
+        cell.onRetry = loadImage
+        loadImage()
+        
+        return cell
+    }
+    
+    func preload() {
+        task = imageLoader.loadImageData(from: model.url) { _ in }
+    }
+    
+    deinit {
+        task?.cancel()
+    }
+}
