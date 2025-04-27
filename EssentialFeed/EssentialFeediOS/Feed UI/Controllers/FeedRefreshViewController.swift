2958d0be435816083a0e5cb93e8761fb042c26ef
Fernando Campo <campofernando594@gmail.com>
2025-04-27 09:17:13 -0300

    Create FeedViewModel for isolating feed loading logic and keeping the state of the view

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,6 +8,54 @@
 import UIKit
 import EssentialFeed
 
+final class FeedViewModel {
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    private enum State {
+        case pending
+        case loading
+        case loaded([FeedImage])
+        case failed
+    }
+    
+    private var state: State = .pending {
+        didSet{
+            onChange?(self)
+        }
+    }
+    
+    var onChange: ((FeedViewModel) -> Void)?
+    
+    var isLoading: Bool {
+        switch state {
+        case .loading: return true
+        case .pending, .loaded, .failed: return false
+        }
+    }
+    
+    var feed: [FeedImage]? {
+        switch state {
+        case .loaded(let feed): return feed
+        case .pending, .loading, .failed: return nil
+        }
+    }
+    
+    func loadFeed() {
+        state = .loading
+        feedLoader.load { [weak self] result in
+            if case .success(let feed) = result {
+                self?.state = .loaded(feed)
+            } else {
+                self?.state = .failed
+            }
+        }
+    }
+}
+
 final public class FeedRefreshViewController: NSObject {
     public lazy var view: UIRefreshControl = {
         let view = UIRefreshControl()
@@ -15,21 +63,26 @@ final public class FeedRefreshViewController: NSObject {
         return view
     }()
     
-    private let feedLoader: FeedLoader
+    private let viewModel: FeedViewModel
     
     init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
+        self.viewModel = FeedViewModel(feedLoader: feedLoader)
     }
     
     var onRefresh: (([FeedImage]) -> Void)?
     
     @objc func refresh() {
-        view.beginRefreshing()
-        feedLoader.load { [weak self] result in
-            if case .success(let feed) = result {
+        viewModel.onChange = { [weak self] viewModel in
+            if viewModel.isLoading {
+                self?.view.beginRefreshing()
+            } else {
+                self?.view.endRefreshing()
+            }
+            
+            if let feed = viewModel.feed {
                 self?.onRefresh?(feed)
             }
-            self?.view.endRefreshing()
         }
+        viewModel.loadFeed()
     }
 }
