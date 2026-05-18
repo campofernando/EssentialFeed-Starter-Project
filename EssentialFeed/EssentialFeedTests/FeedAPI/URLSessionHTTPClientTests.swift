7a0de4e3daef57bd9c6c9f3fd80036e089553e2b
Fernando Campo <campofernando594@gmail.com>
2026-05-18 15:12:45 -0300

    Synchronize access to global 'URLProtocolStub.stub' with a private 'DispatchQueue' to prevent data races

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -152,21 +152,37 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     private class URLProtocolStub: URLProtocol {
-        private static var stub: Stub?
-        private static var requestObserver: ((URLRequest) -> Void)?
-        
         private struct Stub {
             let data: Data?
             let response: URLResponse?
             let error: Error?
+            let requestObserver: ((URLRequest) -> Void)?
         }
         
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get {
+                queue.sync { _stub }
+            }
+            set {
+                queue.sync { _stub = newValue }
+            }
+        }
+
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
+        private static let userInitiatedQueue: OperationQueue = {
+            let q = OperationQueue()
+            q.maxConcurrentOperationCount = 1
+            q.qualityOfService = .userInitiated
+            return q
+        }()
+        
         static func stub(data: Data?, response: URLResponse?, error: Error?) {
-            stub = Stub(data: data, response: response, error: error)
+            stub = Stub(data: data, response: response, error: error, requestObserver: nil)
         }
         
         static func observeRequests(observer: @escaping (URLRequest) -> Void) {
-            requestObserver = observer
+            stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
         
         static func startInterceptingRequests() {
@@ -176,7 +192,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
-            requestObserver = nil
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
@@ -188,24 +203,29 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            if let requestObserver = URLProtocolStub.requestObserver {
-                client?.urlProtocolDidFinishLoading(self)
-                return requestObserver(request)
+            guard let stub = URLProtocolStub.stub else { return }
+
+            URLProtocolStub.userInitiatedQueue.addOperation { [weak self] in
+                guard let self = self else { return }
+
+                if let data = stub.data {
+                    self.client?.urlProtocol(self, didLoad: data)
+                }
+
+                if let response = stub.response {
+                    self.client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+                }
+
+                if let error = stub.error {
+                    self.client?.urlProtocol(self, didFailWithError: error)
+                } else {
+                    self.client?.urlProtocolDidFinishLoading(self)
+                }
+
+                if let observer = stub.requestObserver {
+                    observer(self.request)
+                }
             }
-            
-            if let data = URLProtocolStub.stub?.data {
-                client?.urlProtocol(self, didLoad: data)
-            }
-            
-            if let response = URLProtocolStub.stub?.response {
-                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-            }
-            
-            if let error = URLProtocolStub.stub?.error {
-                client?.urlProtocol(self, didFailWithError: error)
-            }
-            
-            client?.urlProtocolDidFinishLoading(self)
         }
         
         override func stopLoading() {}
